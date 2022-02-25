89886903568666129a895168e3ed61f3127cb5fd
Achu Ajesh <achuv37@gmail.com>
2022-02-25 10:55:09 -0500

    Declared Variables

--- Develop/script.js
+++ Develop/script.js
@@ -7,6 +7,15 @@
 // 5. Generate password based on the criteria.
 
 
+// Declaring variables.
+
+var number = [0,1,2,3,4,5,6,7,8,9];
+var upperCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var specialCharacters = ["@","#","!","$","%","^","&","*","(",")","_","-","+","=","{","}","[","]",":",";","<",">","?"];
+var passwordLength ;
+var preferences;
+
 
 
 
